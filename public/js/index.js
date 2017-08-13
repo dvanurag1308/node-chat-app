8aba169634825e912cdad60aec29297414829c29
dvanurag1308 <dvanurag1308@gmail.com>
2017-08-13 18:31:16 -0400

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();
+socket.on('connect', function() {
+    console.log('connected to server');
+
+    socket.emit('createMessage', {
+        from: 'anurag',
+        text: 'im good'
+    });
+});
+
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+});
+
+socket.on('newMessage', function(message){
+    console.log('new message', message);
+});
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
+
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
@@ -14,9 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'andy',
+        text: 'wassup',
+        createdAt: 234
+    });
+
+    socket.on('createMessage', function(message){
+        console.log('New Message', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
-    })
+    });
 });
 
 
