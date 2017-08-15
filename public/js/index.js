dfd00bf6e54a3730ed72ec27584eedd9369e104e
dvanurag1308 <dvanurag1308@gmail.com>
2017-08-14 23:09:00 -0400

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function(message){
 jQuery("#message-form").on('submit', function(e){
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function(){
-
+        messageTextbox.val('');
     });
 });
 
@@ -42,13 +44,16 @@ locationButton.on('click', function(e){
     if(!navigator.geolocation){
         return alert('Geolocation not supported by the browser');
     }
-
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
     navigator.geolocation.getCurrentPosition(function(position){
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, function(){
+            locationButton.removeAttr('disabled').text('Send Location');
         });
     }, function() {
         alert('Unable to access location');
+        locationButton.removeAttr('disabled').text('Send Location');
     });
 });
