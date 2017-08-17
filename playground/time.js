78454f665ed18c1eefdd1a18691d7e3692cb0fe1
dvanurag1308 <dvanurag1308@gmail.com>
2017-08-16 21:53:29 -0400

    format with Moment JS

---
+++ playground/time.js
@@ -0,0 +1,16 @@
+var moment = require('moment');
+
+
+// var date = new Date();
+
+// console.log(date);
+
+// var date = moment();
+
+// date.add(5, 'years').subtract(9, 'months')
+
+// console.log(date.format('MMM Do, YYYY'))
+
+var date = moment();
+
+console.log(date.format('h:mm:ss a'))
